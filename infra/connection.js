46ee23247488269e61d77b45c21741971c49b4bf
João Vitor Novais <joaoeuno2011@gmail.com>
2020-06-02 18:52:33 +0000

    adicionando e testando servidor mongo

--- infra/connection.js
+++ infra/connection.js
@@ -1,7 +1,11 @@
 //require
 const mongoose = require('mongoose');
+
+require('dotenv').config({ path: '.env' });
+
 //configurações de conexao
-const connection = mongoose.connect("mongodb://localhost/livbri",{
+
+const connection = mongoose.connect("mongodb+srv://"+process.env.USER_DB+":<"+process.env.PASSWORD_DB+">@cluster0-ydowt.gcp.mongodb.net/test?retryWrites=true&w=majority",{
     useUnifiedTopology: true,
     useNewUrlParser:true
 })
